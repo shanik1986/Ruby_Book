fd2eed5e8ff45103a44057d3fc817dbf364f63d2
Shani Keynan - GIT User <shanik1986@gmail.com>
2016-11-02 19:16:42 +0000

    Variables: Ex.2

---
+++ vars/name.rb
@@ -0,0 +1,6 @@
+# Variables: Ex. 1 - Usert types in name and the prog prints a welcome message with the name
+
+puts "Hey there, what's your name?"
+name = gets.chomp
+
+puts "Welcome to this stupid program #{name}"
