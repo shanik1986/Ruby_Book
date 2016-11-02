11d9a3a6245d68fb84a27498c84249806c857c2f
Shani Keynan - GIT User <shanik1986@gmail.com>
2016-11-02 14:36:38 +0000

    The Basics: Ex.3

---
+++ basics/ex3.rb
@@ -0,0 +1,8 @@
+#The basics: Excercise 3 - Hash of movies titles and year they came out
+
+a = {:'King Kong' => 1700, :'Terminator' => 1999, :'Aba Ganuv' => 1980}
+
+puts a[:'King Kong']
+puts a[:'Terminator']
+puts a[:'Aba Ganuv']
+
