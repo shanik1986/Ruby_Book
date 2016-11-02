b019554f0413f0c8cb717a1a40612212dac21a9d
Shani Keynan - GIT User <shanik1986@gmail.com>
2016-11-02 19:20:00 +0000

    Variables: Ex.3

--- vars/name.rb
+++ vars/name.rb
@@ -1,6 +1,10 @@
-# Variables: Ex. 1 - Usert types in name and the prog prints a welcome message with the name
+# Variables: Ex. 1 + 3 - Usert types in name and the prog prints a welcome message with the name. Then, it prints your name 10 times
 
 puts "Hey there, what's your name?"
 name = gets.chomp
 
 puts "Welcome to this stupid program #{name}"
+
+10.times do |n|
+  puts name
+end
