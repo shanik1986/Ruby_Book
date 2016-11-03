2e579f3b414af619963e95edb4318e2ba4a292fd
Shani Keynan - GIT User <shanik1986@gmail.com>
2016-11-03 20:16:18 +0000

    Loops and Iterations: Ex.2 Correction to while loop

--- loops/ex2.rb
+++ loops/ex2.rb
@@ -1,11 +1,11 @@
 # Loopes and Iterations: Ex.2 - While loop that takes string from user and stops when he types STOP
 
+u_input = ""
 
-loop do
+while u_input != 'STOP' do
   puts "Do you have something to say?"
   u_input = gets.chomp
-  if u_input == 'STOP'
-    puts "Ok, I'll stop"
-    break
-  end
 end
+
+puts "Ok, I'll stop"
+  
