b496c660a12965dd8d641007df9810650038471f
Shani Keynan - GIT User <shanik1986@gmail.com>
2016-11-05 21:49:25 +0000

    Arrays: Ex. 7

---
+++ arrays/ex7.rb
@@ -0,0 +1,7 @@
+# Arrays: Ex.7 - Iterates over array and creates a new one which is the result of incrementing each item by 2
+
+arr1 = [1, 2, 3, 4, 5, 6]
+arr2 = arr1.map {|num| num + 2}
+
+p arr1
+p arr2
